7fd3608306a83f9201d36717e02d540f60d3e5f8
logzinga <65839350+logzinga@users.noreply.github.com>
2022-12-30 13:53:32 +1100

    join and leave class

---
+++ src/main/java/com/logzinga/joinandleave/JoinLeaveListener.java
@@ -0,0 +1,14 @@
+package com.logzinga.joinandleave;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class JoinLeaveListener implements Listener {
+
+    @EventHandler
+    public void onLeave(PlayerQuitEvent e){
+
+    }
+
+}
