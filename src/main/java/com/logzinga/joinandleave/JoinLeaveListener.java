04c68524a26bced16b6b4fc0020434b4544b1fa3
logzinga <65839350+logzinga@users.noreply.github.com>
2022-12-30 14:12:01 +1100

    join message has been added for both new and returning players

--- src/main/java/com/logzinga/joinandleave/JoinLeaveListener.java
+++ src/main/java/com/logzinga/joinandleave/JoinLeaveListener.java
@@ -4,9 +4,21 @@ import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class JoinLeaveListener implements Listener {
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e){
+        Player player = e.getPlayer();
+
+        if (player.hasPlayedBefore()){
+            e.setJoinMessage(ChatColor.YELLOW + player.getDisplayName() + " has returned to hell");
+        }else{
+            e.setJoinMessage(ChatColor.YELLOW + player.getDisplayName() + " has joined for the first time, find them.");
+        }
+    }
 
     @EventHandler
     public void onLeave(PlayerQuitEvent e){
